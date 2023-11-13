29dfd8524a43164733e99f8270cceeb83b22d98a
Denise <88467086+moncadad@users.noreply.github.com>
2023-11-13 16:21:35 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1>This one with build project folder only.</h1>
+      <h1>This one with build project and original folder.</h1>
     </>
   );
 }
